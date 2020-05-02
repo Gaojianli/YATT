cb0096c96cb872e02a6c3f937f7b2f9b401a0c9f
Gaojianli <823805669hehe@gmail.com>
2020-05-02 19:54:49 +0800

    Initial commit

---
+++ app/src/main/java/me/gaojianli/yetanothertiktok/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.gaojianli.yetanothertiktok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.gaojianli.yetanothertiktok.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/me/gaojianli/yetanothertiktok/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package me.gaojianli.yetanothertiktok.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
