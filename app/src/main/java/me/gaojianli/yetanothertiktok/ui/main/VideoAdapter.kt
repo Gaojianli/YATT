d8c0393346ca47b6cea4344e20caa1ca8f82a8ff
Gaojianli <823805669hehe@gmail.com>
2020-05-03 18:07:38 +0800

    Finish avatar loading

--- app/src/main/java/me/gaojianli/yetanothertiktok/ui/main/VideoAdapter.kt
+++ app/src/main/java/me/gaojianli/yetanothertiktok/ui/main/VideoAdapter.kt
@@ -1,18 +1,26 @@
 package me.gaojianli.yetanothertiktok.ui.main
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import me.gaojianli.yetanothertiktok.BR
 import me.gaojianli.yetanothertiktok.R
 import me.gaojianli.yetanothertiktok.data.VideoResponse
 import me.gaojianli.yetanothertiktok.databinding.VideoItemBinding
 
-class VideoAdapter(private val videoList: List<VideoResponse>) :
+class VideoAdapter(private val videoList: List<VideoResponse>, private val mContext: Context) :
     RecyclerView.Adapter<VideoAdapter.Companion.VideoViewHolder>() {
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
         holder.binding.setVariable(BR.videoItem, videoList[position])
+        val avatarView: ImageView = holder.itemView.findViewById(R.id.avatar_img)
+        Glide.with(mContext)
+            .load(videoList[position].avatarUrl)
+            .placeholder(R.mipmap.test_image)
+            .into(avatarView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
