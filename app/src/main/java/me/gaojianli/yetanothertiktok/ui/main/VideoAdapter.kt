a9e10e2aeafe71e48184f32d7614daf3fdaa630c
Gaojianli <823805669hehe@gmail.com>
2020-05-04 17:12:41 +0800

    Smooth the animate

--- app/src/main/java/me/gaojianli/yetanothertiktok/ui/main/VideoAdapter.kt
+++ app/src/main/java/me/gaojianli/yetanothertiktok/ui/main/VideoAdapter.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import me.gaojianli.yetanothertiktok.BR
@@ -17,20 +18,22 @@ import me.gaojianli.yetanothertiktok.databinding.VideoItemBinding
 import java.io.ByteArrayOutputStream
 
 class VideoAdapter(
-    private val videoList: List<VideoResponse>,
-    private val previewMap: Map<String, Bitmap>,
+    private val liveVideoList: LiveData<List<VideoResponse>>,
+    previewMap: Map<String, Bitmap>,
     private val mContext: Context
 ) :
     RecyclerView.Adapter<VideoAdapter.Companion.VideoViewHolder>() {
+    private var videoList: List<VideoResponse> = ArrayList<VideoResponse>()
+    private var mPreviewMap: Map<String, Bitmap> = previewMap
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
         holder.binding.setVariable(BR.videoItem, videoList[position])
         val cardView: CardView = holder.itemView.findViewById(R.id.list_item_cardview)
         cardView.setOnClickListener {
             val intent = Intent(mContext, PlayActivity::class.java)
             intent.putExtra("videoInfo", videoList[position])
-            if (previewMap.containsKey(videoList[position].id) && previewMap[videoList[position].id] != null) {
+            if (mPreviewMap.containsKey(videoList[position].id) && mPreviewMap[videoList[position].id] != null) {
                 val byteArrayOutputStream = ByteArrayOutputStream()
-                previewMap[videoList[position].id]?.compress(
+                mPreviewMap[videoList[position].id]?.compress(
                     Bitmap.CompressFormat.PNG,
                     100,
                     byteArrayOutputStream
@@ -44,9 +47,9 @@ class VideoAdapter(
             .load(videoList[position].avatarUrl)
             .placeholder(R.mipmap.default_avatar)
             .into(avatarView)
-        if (previewMap.containsKey(videoList[position].id) && previewMap[videoList[position].id] != null)
+        if (mPreviewMap.containsKey(videoList[position].id) && mPreviewMap[videoList[position].id] != null)
             holder.itemView.findViewById<ImageView>(R.id.video_preview)
-                .setImageBitmap(previewMap[videoList[position].id])
+                .setImageBitmap(mPreviewMap[videoList[position].id])
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
@@ -61,6 +64,12 @@ class VideoAdapter(
     }
 
     override fun getItemCount(): Int = videoList.size
+
+    fun refresh(previewMap: Map<String, Bitmap>) {
+        videoList = liveVideoList.value ?: ArrayList()
+        mPreviewMap = previewMap
+        notifyDataSetChanged()
+    }
 
     companion object {
         class VideoViewHolder(val binding: VideoItemBinding) :
